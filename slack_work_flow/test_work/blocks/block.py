f3ea12384348879f006b6be156a95690a979b809
pochinoritaro <pochimoritaro@gmail.com>
2024-03-28 19:31:26 +0900

    切り分けテストとして/testコマンドを実装 #3

---
+++ slack_work_flow/test_work/blocks/block.py
@@ -0,0 +1,12 @@
+
+def test_block():
+    return [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "This is a plain text section block.",
+                        "emoji": True
+                    }
+                }
+            ]
